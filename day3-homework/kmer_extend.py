329ae5b641924a0666233ea2f99e1c6589af1d0f
Justin Mirazee <jmirazee@jhu.edu>
2019-08-29 00:40:15 -0400

    corrections, comments added

--- day3-homework/kmer_extend.py
+++ day3-homework/kmer_extend.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
 
 """
-match all kmers in a FASTA file
-
-#subset.fa is our target
-# the dro is query
-
-
-1. I ran fasta.py on the fasta sequence so that I could parse it
-2. 
-
-
 [usage statement]: enter this argument:
 ./.py file subset.fa droYak2_seq.fa 11
  script, target file, query file, and kmer number
@@ -21,10 +11,11 @@ import sys
 target = FASTAReader(open(sys.argv[1])) # classes are generally camel-case
 query = FASTAReader(open(sys.argv[2]))
 k = int(sys.argv[3]) # command line prompt will tell the number of units it's looking to match (e.g., 4, 11)
+
+#basically do same thing as excercise 1 here
 target_dictionary = {}
-name_dictionary = {}
+
 for name_t, sequence_t in target:
-   name_dictionary[name_t] = sequence_t
    for i in range(0, len(sequence_t) - k + 1):
        kmer_t = sequence_t[i:i+k].upper()
        target_tuple = (name_t, i)
@@ -32,7 +23,11 @@ for name_t, sequence_t in target:
            target_dictionary[kmer_t].append(target_tuple)
        else:
            target_dictionary[kmer_t] = [target_tuple]
+        
+#initialize new list that will contain the final info
 extended_kmers = []
+
+
 for name_q, sequence_q in query:
    for j in range(0, len(sequence_q) - k + 1):
        kmer_q = sequence_q[j:j+k].upper()
@@ -40,60 +35,41 @@ for name_q, sequence_q in query:
            continue
        else:
            for target_tuple in target_dictionary[kmer_q]:
-               #print(target_tuple, j, kmer_q)
-               x = 1
+
+#now instead of print, move on by starting a while loop to extend kmers. 
+#start "x" counter at 1.
+
+#we only need to increase the size of the kmer from the righr!
+
+#J = index of query
+#I = index of target file
+
+               x = 1 
                new_kmer_q = kmer_q
                while True:
                    if j-x == 0 or i-x == 0:
                        break
+                       #why needed? Because this would mean we are at beginning and don't need to go further.
+                       
+                       #if letter in query = letter in target, add to new string.
                    if sequence_q[j-x] == sequence_t[i-x]:
                        new_kmer_q = sequence_q[j-x].join(new_kmer_q)
                        x += 1
                    else:
                        break
-               x = 1
-               while True:
-                   if j+k+x >= (len(sequence_q) - 1) or i+k+x >= (len(sequence_t) - 1):
-                       break
-                   if sequence_q[j+k+x] == sequence_t[i+k+x]:
-                       new_kmer_q = "".join(new_kmer_q, sequence_q[j-x])
-                       x += 1
-                   else:
-                       break
-               extended_kmers.append(new_kmer_q)
-extended_kmers.sort(reverse = True, key = len)
+                       
+            #don't actually need the commented out things below, for left       
+               # x = 1
+#                while True:
+#                    if j+k+x >= (len(sequence_q) - 1) or i+k+x >= (len(sequence_t) - 1):
+#                        break
+#                    if sequence_q[j+k+x] == sequence_t[i+k+x]:
+#                        new_kmer_q = "".join(new_kmer_q, sequence_q[j-x])
+#                        x += 1
+#                    else:
+#                        break
+               extended_kmers.append(new_kmer_q) #put new_kmer_q string in list?
+extended_kmers.sort(reverse = True, key = len) #sort list
 for new_kmer_q in extended_kmers:
-   print(new_kmer_q)
+   print(new_kmer_q) 
                     
-            
-            # for each kmer, we want to show name_T, i, i, kmer_q 
-            # kmer1
-        
-        
-        
-        
-# for value_q, sequence_q in query:
-#     for i in range(0, len(sequence_q) - k + 1):
-#
-#         kmer_q = sequence_q[i:i+k]
-#
-#
-#
-# if kmer_q == kmer_t:
-#     empty_list.append(kmer_t, kmer_q)
-#
-#
-#
-#
-#
-#         if kmer in kmers:
-#             kmers[kmer]+=1
-#         else:
-#             kmers[kmer] = 1
-#
-# for kmer,count in kmers.items():
-#     print(kmer, count, sep="\t")
-#
-#
-# # ./count_kmers.py 11  < subset.fa | less -S
-#
