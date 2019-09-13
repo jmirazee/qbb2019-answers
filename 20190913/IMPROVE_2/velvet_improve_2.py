c8588e1a725971e116bea1b0a6faf8820ecb55c3
Justin Mirazee <jmirazee@jhu.edu>
2019-09-13 18:38:50 -0400

    improve 2 assemblies without contig metrics

---
+++ 20190913/IMPROVE_2/velvet_improve_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+list_y = []
+list_x = []
+for i, line in enumerate(open(sys.argv[1])):
+    if i == 0:
+        continue
+    fields = line.rstrip("\n").split("\t")
+    y = int(fields[5]) - int(fields[4])
+    x = int(fields[8])
+    list_y.append(y)
+    list_x.append(x)
+#print(list_y)   
+
+fig, ax = plt.subplots() #can have more than two, for figure a,b,c,et cetera
+fig.suptitle("Dotplot_Velvet_improve_2")
+
+ax.scatter(x=list_x, y=list_y, alpha = 0.2)
+ax.set_xlabel("total len of contigs")
+ax.set_ylabel("len of reference")
+
+
+fig.savefig("Dotplot_Velvet_improve_2.png")
+plt.close(fig)
