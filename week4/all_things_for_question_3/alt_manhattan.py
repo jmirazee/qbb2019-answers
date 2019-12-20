aa8a0f6d6483def7fb88b58a9712a4e41e4fb70f
Justin Mirazee <jmirazee@jhu.edu>
2019-12-20 01:47:38 -0500

    question3

---
+++ week4/all_things_for_question_3/alt_manhattan.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+#os.listdir on os.gitcwb
+colors = ['red', 'blue']
+for file_name in os.listdir (os.getcwd()):
+    if file_name.endswith('.qassoc'):
+        pval_by_chr = {}
+        qassoc_file = open(file_name)
+        for i, line in enumerate(qassoc_file):
+            if i == 0:
+                continue
+            fields = line.rstrip("\n").split()
+            chromo = fields[0]
+            p_val = fields[-1]
+            if p_val == 'NA':
+                continue
+            pval_by_chr.setdefault(chromo,[])
+            pval_by_chr[chromo].append(-1*np.log10(float(p_val)))
+        fig, ax = plt.subplots()
+        plt.tick_params(axis= "x", which= "both", bottom=False, top=False, labelbottom=False)
+        previous_points = 0
+        for i, chromo in enumerate(pval_by_chr):
+            ax.scatter([x+previous_points for x in range(len(pval_by_chr[chromo]))], pval_by_chr[chromo], color = colors[i%2])
+            previous_points += len(pval_by_chr[chromo]) 
+        ax.set_xlabel("Chromosomes")
+        ax.set_ylabel("Frequency")
+        ax.set_title(file_name)
+        fig.savefig(file_name+'.png')
+        plt.close()
